c19b6d62348bd15a866727c3b21c321017763ef8
Dofmor <danof.mora@gmail.com>
2022-02-05 21:51:45 -0800

    First commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Daniel Morales on 2/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
