cd4a071ea03e4ae5cde2cdbcab223a69b62dac66
Dofmor <danof.mora@gmail.com>
2022-02-07 22:48:47 -0800

    Unit 2 user stories

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Daniel Morales on 2/7/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
